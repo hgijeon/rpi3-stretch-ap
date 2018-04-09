481ef20a5b16875427a5939e2f0fed26bb756575
Henry <henry@polariant.io>
2018-04-09 20:55:37 +0900

    explain argument of install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-sudo basic-setup.sh && sudo cp dhcpcd.sh /usr/lib/dhcpcd5/dhcpcd
+
+if [ "$EUID" -ne 0 ]
+then echo "Must be root"
+     exit
+fi
+
+if [[ $# -lt 2 ]]; 
+then echo "You need to pass a password and name!"
+     echo "Usage:"
+     echo "sudo $0 [apPassword] [apName]"
+     exit
+fi
+
+
+
+basic-setup.sh $1 $2 && cp dhcpcd.sh /usr/lib/dhcpcd5/dhcpcd
 
